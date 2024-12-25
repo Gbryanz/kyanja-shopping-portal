de7c342facb23a0a63a96b86f7e8d48f8a53863a
Guloba Brian <113831597+Gbryanz@users.noreply.github.com>
2024-12-25 14:26:50 +0300

    Add files via upload
    ## Contact Information  
    For inquiries, feel free to reach out via:  
    - **Phone:** [0750926918](tel:+256750926918) (Available on WhatsApp and direct calls)  
    - **Email:** [gulobabrian@gmail.com](mailto:gulobabrian@gmail.com)

---
+++ about-us.php
@@ -0,0 +1,58 @@
+<?php session_start();
+include_once('includes/config.php');
+error_reporting(0);
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Kyanja Shopping Portal | Category wise Shop </title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+       <!--  <link href="css/bootstrap.min.css" rel="stylesheet" /> -->
+    </head>
+    <body>
+<?php include_once('includes/header.php');?>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">About Us</h1>
+        
+                </div>
+            </div>
+        </header>
+        <!-- Section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+    Agriculture marketing involves the activities and processes used to bring agricultural products from the farm to the consumer. Kyanja Agricultural Resource Center Uganda, located in Kampala's Kawempe Division, plays a pivotal role in this process. 
+
+    Established by the Kampala Capital City Authority (KCCA) in 2013, Kyanja Agricultural Resource Center spans a 30-acre facility. It has emerged as a thriving hub, promoting sustainable agriculture and addressing critical issues such as income generation and food security. The center is designed to enhance market access for farmers, securing better investments in agriculture to improve outcomes, particularly for smallholder farmers.
+
+    At Kyanja, farmers can connect directly with markets, ensuring fair pricing and consistent demand for their produce. This approach guarantees equitable, sustainable, and high-quality farm products for all Ugandans and even extends to neighboring countries. From fresh fruits, vegetables, and grains to innovative agricultural practices, Kyanja is redefining the way agricultural marketing works. 
+
+    With year-round training, events, and access to resources, the center empowers farmers with the tools they need to thrive. Kyanja Agricultural Resource Center is not just a marketplace; it’s a community dedicated to transforming agriculture for a brighter, sustainable future.
+</div>
+
+            </div>
+
+ 
+</div>
+        </section>
+        <!-- Footer-->
+   <?php include_once('includes/footer.php'); ?>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
